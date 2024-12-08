dc641544a7b7228c79b529878c3cb7c5106ee3d6
N1kSt4r <nikita2000tsuranov@gmail.com>
2024-12-08 15:09:39 +0300

    initial

---
+++ listener.py
@@ -0,0 +1,368 @@
+import os
+import cv2
+import time
+import torch
+import numpy as np
+import pyscreenshot as ImageGrab
+
+from threading import Thread
+
+from pynput.mouse import Button, Controller as MouseController
+from pynput.keyboard import Listener, Key, Controller as KeyController
+
+USE_PAUSE = True
+SHIFTS = [-1, 0, 1]
+BORDER_COLOR = [83, 65, 43]
+
+TRIES_SLEEP = 0.123
+TRIES_NUMBER = 10
+BORDER_WIDTH = 4
+CELL_WIDTH = 36
+SCREEN_SCALE = 3
+
+
+def pairwise_L1_distance(left, right):
+    left = np.array(left)[:, None] # n 1 d
+    right = np.array(right)[None]  # 1 m d
+    distances = np.max(np.abs(left - right), axis=-1) # L1 distance
+    return distances
+
+
+class Solver(Listener):
+    def __init__(self):
+        super().__init__(
+            on_press=self._on_press,
+            on_release=self._on_release,
+        )
+        self.miss_count = 0
+        self.terminated = False
+
+        self.mouse = MouseController()
+        self.keyboard = KeyController()
+
+        self.recognizer = torch.load('recognizer.pth', weights_only=False).eval()
+        self.solve_thread = None
+
+        print('Use "q"/"esc" to stop, "s"/"space" to start')
+
+    def _on_release(self, key):
+        if key == Key.esc or getattr(key, 'char', None) == 'q':
+            return False
+
+    def __exit__(self, type, value, traceback):
+        self.join() 
+        super().__exit__(type, value, traceback)
+
+    def _on_press(self, key):
+        char = getattr(key, 'char', None)
+        if char == 'q':
+            self.terminated = True
+        elif char == 's' or key == Key.space:
+            if self.solve_thread is None:
+                self.solve_thread = Thread(target=self.solve, daemon=True)
+                self.solve_thread.start()
+            else:
+                try:
+                    self.solve_thread.join(timeout=0.5)
+                    self.solve_thread = None
+                    self._on_press(key)
+                except:
+                    print('Can\'t join the thread')
+        elif key == Key.f9:
+            pass
+        else:
+            self.miss_count += 1
+            if self.miss_count > 5:
+                print('Use "q"/"esc" to stop, "s"/"space" to start')
+
+    def solve(self):
+        frame = np.array(ImageGrab.grab())
+        self.find_borders()
+
+        self.start_game()
+
+        while not self.terminated:
+            self.mouse.position = 0, 0
+            self.press_key(Key.f9, press_time=1)
+            self.press_key(Key.f9)
+            field = self.get_field()
+            text_field = self.get_text_field(field)
+
+            line_field = ''.join(text_field)
+            g_number = line_field.count('g')
+            if g_number > 10:
+                break
+            elif g_number > 0:
+                continue
+
+            assert 'g' not in line_field
+
+            text_field = self.add_margin(text_field)
+            to_left, to_right = self.find_naive(text_field)
+            to_left_hard, to_right_hard, maybe_cell = self.find_hard(text_field)
+            to_right.update(to_right_hard)
+            # if len(to_left) < 2:
+            to_left += list(to_left_hard)
+
+            if to_left:
+                if 'c' in line_field:
+                    clock_cells = self.find_char(text_field, 'c')
+                    if clock_cells:
+                        to_left = self.trunk_nearest(clock_cells, to_left)
+                elif 'm' in line_field:
+                    mana_cells = self.find_char(text_field, 'm')
+                    if mana_cells:
+                        to_left = self.trunk_nearest(mana_cells, to_left)
+
+                to_left = list(to_left)[:3]
+
+            if to_left or to_right: 
+                self.process_clicks(to_left, to_right, shift=-1)
+            elif maybe_cell:
+                self.shoot(maybe_cell, shift=-1)
+
+    def find_borders(self):
+        for i in range(TRIES_NUMBER):
+            frame = np.array(ImageGrab.grab())
+            mask = np.all(frame == BORDER_COLOR, axis=-1)
+
+            coords = np.vstack(np.where(mask)).T
+
+            # filter outliers
+            distances = pairwise_L1_distance(coords, coords)
+            distances = distances + np.eye(len(distances)) * np.max(distances) # remove zero diag
+            coords = coords[np.min(distances, axis=0) < 5]
+
+            begin = np.min(coords, axis=0) + BORDER_WIDTH
+            end = np.max(coords, axis=0) + 1 - BORDER_WIDTH
+
+            frame = frame[begin[0]: end[0], begin[1]: end[1]]
+            if frame.shape[0] % CELL_WIDTH != 0 or frame.shape[1] % CELL_WIDTH != 0:
+                time.sleep(TRIES_SLEEP)
+                continue
+
+            self.zero = begin
+            self.borders = begin, end
+
+            return
+        raise RuntimeError("check")
+
+    def cell2pixel(self, cell, shift=0):
+        pixel = np.flip(self.zero) + CELL_WIDTH // 2 + (np.flip(cell) + shift) * CELL_WIDTH
+        return tuple(pixel // SCREEN_SCALE)
+
+    def process_clicks(self, to_left, to_right, shift=0):
+        for cell in to_left:
+            self.mouse.position = self.cell2pixel(cell, shift=shift)
+            time.sleep(0.020)
+            self.mouse.click(Button.left, 1)
+            time.sleep(0.005)
+        for cell in to_right:
+            self.mouse.position = self.cell2pixel(cell, shift=shift)
+            time.sleep(0.015)
+            self.mouse.click(Button.right, 1)
+            time.sleep(0.005)
+
+    def press_key(self, key, press_time=0.015):
+        if key != Key.f9 or USE_PAUSE:
+            self.keyboard.press(key)
+            time.sleep(press_time)
+            self.keyboard.release(key)
+            time.sleep(0.050)
+        else:
+            time.sleep(press_time)
+
+    def shoot(self, cell, shift=0):
+        self.press_key('1')
+        self.process_clicks([cell], [], shift=shift)
+        self.press_key(Key.f9, press_time=2.5)
+        self.press_key(Key.f9)
+
+    def start_game(self):
+        frame = self.get_field()
+        text_field = self.get_text_field(frame)
+        center = len(text_field) // 2, len(text_field[0]) //2
+        self.mouse.position = self.cell2pixel(center)
+        self.process_clicks([center], [])
+
+    def get_field(self):
+        self.mouse.position = 0, 0
+        time.sleep(0.050)
+
+        borders = np.hstack(self.borders)
+        borders = tuple(borders[[1, 0, 3, 2]])
+        return np.array(ImageGrab.grab(borders))
+
+    def add_margin(self, field):
+        margin = list(' ' * (len(field[0]) + 2))
+        return [margin] + [list(f' {line} ') for line in field] + [margin]
+
+    def get_text_field(self, field):
+        symbols = ".cmg *123456"
+        tensor = torch.as_tensor(field, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0)
+        with torch.no_grad():
+            predict = torch.argmax(self.recognizer(tensor)[0], 0).tolist()
+
+        text_field = []
+        for row in predict:
+            str_row = []
+            for idx in row:
+                str_row.append(symbols[idx])
+            text_field.append(''.join(str_row))
+        return text_field
+
+    def is_unknown(self, char):
+        return char in ".cm"
+
+    def trunk_nearest(self, cells, candidates, count=100500):
+        candidates = np.array(list(candidates))
+        distances = np.min(pairwise_L1_distance(candidates, cells), axis=-1)
+        idxs = np.argsort(distances)[:count]
+        return candidates[idxs].tolist()
+
+    def find_char(self, field, char):
+        cells = []
+        for i in range(1, len(field) - 1):
+            for j in range(1, len(field[0]) - 1):
+                if field[i][j] == char:
+                    cells.append((i, j))
+        return cells
+
+    def find_naive(self, field):
+        to_left, to_right = set(), set()
+
+        for i in range(1, len(field) - 1):
+            for j in range(1, len(field[0]) - 1):
+                if '1' <= field[i][j] <= '8':
+                    mines_max = int(field[i][j])
+                    mines_count = 0
+                    closed = []
+
+                    for shift_i in SHIFTS:
+                        for shift_j in SHIFTS:
+                            if self.is_unknown(field[i + shift_i][j + shift_j]):
+                                closed.append((i + shift_i, j + shift_j))
+                            elif field[i + shift_i][j + shift_j] == '*':
+                                mines_count += 1
+
+                    if mines_count == mines_max and closed:
+                        to_left.add((i, j, len(closed)))
+                    elif mines_count + len(closed) == mines_max:
+                        to_right.update(set(closed))
+
+        to_left = sorted(to_left, key=lambda x: -x[-1])
+        to_left = [cell[:2] for cell in to_left]
+        return to_left, to_right
+
+    def find_hard(self, field):
+        border, to_check = self.find_stoke(field)
+        count = [0] * len(border)
+
+        self.succes_count = 0
+        self.permute_stroke(field, to_check, border, count, 0)
+
+        min_left_count = 1000
+        min_left_idx = 0
+
+        left_click = []
+        right_click = []
+        for i in range(len(count)):
+            if count[i] < min_left_count:
+                min_left_count = count[i]
+                min_left_idx = i
+            if count[i] == 0:
+                left_click.append(border[i])
+            elif count[i] == self.succes_count:
+                right_click.append(border[i])
+
+        maybe_cell = border[min_left_idx] if min_left_idx < len(border) else None
+
+        return left_click, right_click, maybe_cell
+
+    def is_stroke(self, field, to_check, i, j):
+        flag = False
+        if self.is_unknown(field[i][j]):
+            for shift_i in SHIFTS:
+                for shift_j in SHIFTS:
+                    if field[i + shift_i][j + shift_j].isnumeric():
+                        to_check.add((i + shift_i, j + shift_j))
+                        flag = True
+        return flag
+
+    def find_stoke(self, field):
+        to_check = set()
+        border = set()
+
+        for i in range(1, len(field) - 1):
+            for j in range(1, len(field[0]) - 1):
+                if self.is_stroke(field, to_check, i, j):
+                    border.add((i, j))
+
+        return sorted(border), sorted(to_check)
+
+    def permute_stroke(self, field, to_check, border, count, idx):
+        if idx == len(border):
+            if self.check_field(field, to_check):
+                self.succes_count += 1
+
+                for idx in range(len(border)):
+                    i, j = border[idx]
+                    if field[i][j] == '*':
+                        count[idx] += 1
+            return
+
+        i, j = border[idx]
+        old_value = field[i][j]
+        field[i][j] = '*'
+        if self.check_cell(field, i, j):
+            self.permute_stroke(field, to_check, border, count, idx + 1)
+        field[i][j] = ' '
+        if self.check_cell(field, i, j):
+            self.permute_stroke(field, to_check, border, count, idx + 1)
+        field[i][j] = old_value
+
+    def check_field(self, field, to_check):
+        for cell in to_check:
+            i, j = cell
+            mines_count = 0
+            mines_max = int(field[i][j])
+
+            for shift_i in SHIFTS:
+                for shift_j in SHIFTS:
+                    if field[i + shift_i][j + shift_j] == '*':
+                        mines_count += 1
+
+            if mines_count != mines_max:
+                return False
+        return True
+
+    def check_cell(self, field, i, j):
+        if field[i][j] in '* ':
+            for shift_i in SHIFTS:
+                for shift_j in SHIFTS:
+                    new_i, new_j = i + shift_i, j + shift_j
+                    if field[new_i][new_j].isnumeric() \
+                            and not self.check_cell(field, new_i, new_j):
+                        return False
+            return True
+        else:
+            mines_max = int(field[i][j])
+            mines_count = 0
+            closed_count = 0
+
+            for shift_i in SHIFTS:
+                for shift_j in SHIFTS:
+                    if self.is_unknown(field[i + shift_i][j + shift_j]):
+                        closed_count += 1
+                    elif field[i + shift_i][j + shift_j] == '*':
+                        mines_count += 1
+
+            if mines_count + closed_count < mines_max:
+                return False
+            if mines_count > mines_max:
+                return False
+            return True
+
+# Collect events until released
+with Solver() as solver:
+    pass
---
+++ train.py
@@ -0,0 +1,62 @@
+import os
+import cv2
+import time
+import torch
+import numpy as np
+
+symbols = ".cmg *123456"
+translate_dict = {char: i for i, char in enumerate(symbols)}
+
+def load_img(path):
+    rgb_img = cv2.imread(path)[..., ::-1].copy()
+    return torch.as_tensor(rgb_img, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0) / 255
+
+def load(dir, name):
+    name = dir + name
+    print('process', name)
+    img = load_img(f'{name}.png')
+    with open(f'{name}.txt') as file:
+        label = [[translate_dict[char] for char in line.strip('\n')] for line in file if line.strip('\n')]
+    label = torch.as_tensor(label, dtype=torch.int64).unsqueeze(0)
+    return img,label
+
+def get_unique_names(dir):
+    return {name.split('.')[0] for name in os.listdir(dir)}
+
+train_dataset = [load('train/', name) for name in get_unique_names('train')]
+val_dataset = [load('val/', name) for name in get_unique_names('val')]
+
+model = torch.nn.Conv2d(3, len(translate_dict), bias=False, stride=36, kernel_size=36)
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-3, weight_decay=1e-2)
+criterion = torch.nn.CrossEntropyLoss()
+
+for i in range(0, 5000):
+    loss = 0
+    optimizer.zero_grad()
+    for image, labels in train_dataset:
+        logits = model(image)
+        loss = loss + criterion(logits, labels) / torch.prod(torch.as_tensor(labels.shape))
+    loss /= len(train_dataset) / 1000
+    loss.backward()
+    optimizer.step()
+
+    if i % 100 == 0:
+        print('train loss:', loss.item())
+        loss = 0
+        for image, labels in val_dataset:
+            with torch.no_grad():
+                logits = model(image)
+                loss = loss + criterion(logits, labels) / torch.prod(torch.as_tensor(labels.shape))
+        print('val loss:', loss.item() / len(val_dataset) * 1000)
+
+frame = load_img('val/1555.png')
+
+model.eval()
+predict = torch.argmax(model(frame)[0], 0).tolist()
+
+for row in predict:
+    for idx in row:
+        print(symbols[idx], end='')
+    print()
+
+torch.save(model, 'recognizer.pth')
